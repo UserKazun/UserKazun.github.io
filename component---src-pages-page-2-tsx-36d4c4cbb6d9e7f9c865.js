567631c653b727a7fe1d103cd99e56c16c952fe8
UserKazun <kazuusjlo8@gmail.com>
2022-02-19 15:51:26 +0900

    Updates

---
+++ component---src-pages-page-2-tsx-36d4c4cbb6d9e7f9c865.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[473],{1922:function(e,t,a){a.r(t);var l=a(7294),n=a(1597),r=a(2755),c=a(9470);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-36d4c4cbb6d9e7f9c865.js.map
